058bc56159b6d88291cc14dded880d572b348f3b
Lucas Deecke <ldeecke@gmail.com>
2016-12-19 16:13:22 +0100

    removed redundant definitions

--- include/utilities/vector_operations.h
+++ include/utilities/vector_operations.h
@@ -7,12 +7,6 @@
 
 namespace utilities {
 
-double sum_maximum(std::vector<double> &a, std::vector<double> &b);
-
-double sum_larger_than(std::vector<double> &a, std::vector<double> &b, double input);
-
-std::vector<double> linspace(double min, double max, int n);
-
 std::vector<double> periodic_efficacy(int max_dpi, int t_per_dpi, double t_on, double t_off,
                                       double efficacy_on, double efficacy_off);
 
--- src/utilities/vector_operations.cpp
+++ src/utilities/vector_operations.cpp
@@ -4,43 +4,6 @@
 
 namespace utilities {
 
-double sum_maximum(std::vector<double> &a,std::vector<double> &b)
-{
-    auto i1 = a.begin(), i2 = b.begin();
-    double m = *i1++ + *i2++;
-
-    for (; i1 != a.end(); ++i1, ++i2) {
-        m = std::max(m, *i1 + *i2);
-    }
-
-    return m;
-}
-
-double sum_larger_than(std::vector<double> &a, std::vector<double> &b, double x)
-{
-    auto i1 = a.begin(), i2 = b.begin();
-    int r = 0;
-
-    for (; i1 != a.end(); i1++, i2++) {
-        if (*i1 + *i2 > x) { r += 1; }
-    }
-
-    return static_cast<double>(r);
-}
-
-std::vector<double> linspace(double min, double max, int n)
-{
-    std::vector<double> ls;
-    double increment = (max-min) / (n-1);
-
-    while (min <= max) {
-        ls.push_back(min);
-        min += increment;
-    }
-
-    return ls;
-}
-
 std::vector<double> periodic_efficacy(int total_dpi, int steps_per_dpi, double t_on, double t_off, double efficacy_on, double efficacy_off)
 {
     bool b = true;
