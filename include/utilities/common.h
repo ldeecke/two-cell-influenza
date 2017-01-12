847e07857f75995889f0e697288ae5c1788cacd4
Lucas Deecke <ldeecke@gmail.com>
2017-01-12 19:19:29 +0100

    templated utility function

--- include/utilities/common.h
+++ include/utilities/common.h
@@ -9,7 +9,19 @@
 
 namespace utilities {
 
-void write_result(const std::vector<double>& V_wt, const std::vector<double>& V_mu, std::ofstream& stream);
+const char default_delimiter = ',';
+
+template<typename T>
+void write_result(const std::vector<T>& a, const std::vector<T>& b, std::ofstream& stream)
+{
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        stream << std::to_string(a[i]);
+        stream << default_delimiter;
+        stream << std::to_string(b[i]);
+        stream << std::endl;
+    }
+};
 
 } // namespace utilities
 
